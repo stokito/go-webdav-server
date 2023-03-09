ef3f4785e706b441b69d33ecc86f2de960f99743
Sergey Ponomarev <stokito@gmail.com>
2023-03-09 12:36:44 +0200

    Patched Go webdav without XML

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"go-webdav-serv/webdav2"
+	_ "golang.org/x/net/webdav"
+	"log"
+	"net/http"
+)
+
+func main() {
+	dav := &webdav2.Handler{
+		FileSystem: webdav2.Dir("./"),
+		LockSystem: webdav2.NewMemLS(),
+		Logger: func(r *http.Request, err error) {
+			if err != nil {
+				log.Printf("WEBDAV [%s]: %s, ERROR: %s\n", r.Method, r.URL, err)
+			} else {
+				log.Printf("WEBDAV [%s]: %s \n", r.Method, r.URL)
+			}
+		},
+	}
+	http.ListenAndServe(":8090", dav)
+}
